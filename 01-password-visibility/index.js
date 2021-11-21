5c911782c8cd9c06c07cfeae8cc26b9fd66cc5f2
Jan Sygnowski <jansyg10@gmail.com>
2021-11-21 16:17:33 +0100

    small refactor 01

--- 01-password-visibility/index.js
+++ 01-password-visibility/index.js
@@ -4,6 +4,8 @@ const showPassword=document.querySelector('#show-password')
 let counter=0;
 showPassword.addEventListener('click',()=>{
 //console.log(showPassword);
+/*
+password.type = showPassword.checkled ? 'text' : 'password'*/
   if(showPassword.checked){
     password.type='text';
   }
