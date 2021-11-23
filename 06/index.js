44a3de4376d97e04e5c1b48e8329a348967eb635
Jan Sygnowski <jansyg10@gmail.com>
2021-11-23 15:47:39 +0100

    finished random quotes withour duplicates

--- 06/index.js
+++ 06/index.js
@@ -1,24 +1,38 @@
 //fetch("https://jsonplaceholder.typicode.com/posteds")
+/*Random Ron no duplicates*/
 
-const blockquotes=document.querySelector('.qoute');
-const button=document.querySelector("#get-quote")
+const blockquotes = document.querySelector(".qoute");
+const button = document.querySelector("#get-quote");
 console.log(blockquotes);
 console.log(button);
-button.addEventListener('click',()=>{
+
+const allQuotes = [];
+console.log(allQuotes);
+
+const getQuote=()=>{
   fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
-  .then((e) => {
-    if (e.ok) {
+  .then(e=>{
+    if(e.ok){
       return e.json();
     }
-    return e.json().then((json) => {
-      throw json;
-    });
+    throw e.status;
   })
-  .then((data) => {
-    blockquotes.textContent=data;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then(data=>{
+    if(allQuotes.length>25){
+      allQuotes.shift(); //delete first element
+    }
 
-})
+    if(allQuotes.includes(data[0])){
+      getQuote();
+    }
+    else{
+      allQuotes.push(data[0]);
+      blockquotes.textContent=data;
+    }
+  })
+  .catch(error=>{
+    console.log(error);
+  })
+}
+
+button.addEventListener("click", getQuote)
