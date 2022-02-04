be761cef828b9d33f90e2029e1244767f1d31f13
Jan Sygnowski <jansyg10@gmail.com>
2022-02-04 11:14:33 +0100

    refactor and added style 02

--- 02-multiple-password/index.js
+++ 02-multiple-password/index.js
@@ -1,18 +1,8 @@
-//const currentPassword=document.querySelector('#current-password');
 const passwords = document.querySelectorAll("input[type='password']");
-console.log(passwords);
-//const newPassword=document.querySelector('#new-password');
 const showPasswords = document.querySelector("#show-passwords");
-console.log(showPasswords);
+
 showPasswords.addEventListener("click", () => {
-  if (showPasswords.checked) {
-    passwords.forEach((element) => {
-      console.log(element);
-      element.type = "text";
-    });
-  } else {
-    passwords.forEach((element) => {
-      element.type = "password";
-    });
-  }
+  passwords.forEach((element)=>{
+    element.type=showPasswords.checked ? 'text' : 'password';
+  })
 });
