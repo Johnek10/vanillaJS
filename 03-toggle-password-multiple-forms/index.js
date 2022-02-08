023fe3abe92c5a645c9e98c1fd4ef68278d9dbe9
Jan Sygnowski <jansyg10@gmail.com>
2022-02-08 17:07:57 +0100

    imporeved style 03

--- 03-toggle-password-multiple-forms/index.js
+++ 03-toggle-password-multiple-forms/index.js
@@ -1,32 +1,16 @@
-/* const passwordOld = document.querySelector("#password");
-const passwords = document.querySelectorAll("[data-pass='OK']");
-const userChange = document.querySelector("#show-password");
-
-const passwordChange = document.querySelector("#show-passwords"); */
-
-document.addEventListener('click',(event)=>{
-  if(!event.target.matches('[data-pass]')){
-    console.log(event.target);
-    //in label working, 
-    console.log('dup');
-    return
-  } 
-
-  console.log(event.target.dataset['pass'])
-  const passwordField=event.target.dataset['pass'];
-  const password=document.querySelectorAll(passwordField);
-  password.forEach((e)=>{
-    if(event.target.checked){
-      e.type='text';
+document.addEventListener("click", (event) => {
+  if (!event.target.matches("[data-pass]")) {
+    return;
+  }
+  const passwordField = event.target.dataset["pass"];
+  // console.log(passwordField);
+  const password = document.querySelectorAll(passwordField);
+  //console.log(password)
+  password.forEach((e) => {
+    if (event.target.checked) {
+      e.type = "text";
+    } else {
+      e.type = "password";
     }
-    else{
-      e.type='password';
-    }
-  })
-/*    console.log(password);
-  console.log(passwordField);  */
-
-}
-  )
-
-
+  });
+});
